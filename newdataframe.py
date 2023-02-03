8e8aa9031faa38086223a7774487f32bec3ed5f1
dorisavedikian <m>
2023-02-02 20:59:04 -0800

    update

--- newdataframe.py
+++ newdataframe.py
@@ -11,9 +11,9 @@ def createnewdf():
     df = pd.read_csv('/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/nalt_labels_DATA/nalt_altlabels_1.csv')    
     for (index,row) in df.iterrows():                                                                   
         if df['NALT_URI'].empty:                                                  
-            NALT_URI2 = df['NALT_URI'].iloc[-1]                                                 
+            NALT_URI2 = df['NALT_URI'].iloc[-1]        # this is wrong                          
         else:
-            NALT_URI, Label = (index,row).split(',')            
+            NALT_URI, Label = (index,row).split(',')   # this is wrong            
             df2['NALT_URI'].append(NALT_URI)
             df2['Label'].append(Label)
             df2['NALT_URI2'].append(NALT_URI2)
