09896f12e87df07bda1094d08ce80643e440b018
dorisavedikian <m>
2023-02-08 10:41:12 -0800

    update folders

---
+++ Reconcile-CSV Process/Python scripts/nalt_altlabels.py
@@ -0,0 +1,30 @@
+
+'''
+ - I used Tom Bakers nalt_label.ipynb to create this script
+ - This collects all the altlabel values and URI's instead of the altlabels and the preflabels 
+'''
+
+import csv
+from pathlib import Path
+from rdflib import Graph, SKOS
+
+labels_directory = "nalt_labels_DATA"
+Path(labels_directory).mkdir(exist_ok=True)
+labels_csvfile = Path(labels_directory) / "nalt_altlabels.tsv"
+
+n = Graph()
+n.parse('/Volumes/USDA HD/NAL/MyGitFolder/NALT/nalt4ma/nalt/nalt.nt', format='nt11')
+# n.parse('.../nalt4ma/nalt/nalt.nt', format='nt11')
+
+labels_tuples = []
+
+for predicate in [SKOS.altLabel]:
+    for (concept, label) in n.subject_objects(predicate):
+        if label.language == "en":
+            labels_tuples.append((str(concept), label.value))
+
+with open(labels_csvfile, 'w', newline='') as csvfile:
+    labelwriter = csv.writer(csvfile, delimiter='\t', dialect='excel')
+    labelwriter.writerow(('NALT_URI', 'Label'))
+    for line in sorted(labels_tuples):
+        labelwriter.writerow(line)
