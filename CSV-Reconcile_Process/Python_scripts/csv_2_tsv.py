52e382c4956d13864e3ffa9f7f8296826c3ac911
dorisavedikian <m>
2023-02-14 09:50:48 -0800

    update

--- CSV-Reconcile_Process/Python_scripts/csv_2_tsv.py
+++ CSV-Reconcile_Process/Python_scripts/csv_2_tsv.py
@@ -1,5 +1,14 @@
 import csv
 import pandas as pd
+import os 
 
-df = pd.read_csv('/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/Examples/Cotton/SME labels/CottonLabels.csv')
-df.to_csv("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/Examples/Cotton/SME labels/CottonLabels.tsv", sep="\t", index=False)
+df = pd.read_csv('/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/Examples/Biotech/SME_labels/Biotech_Labels.csv')
+df.to_csv("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/Examples/Biotech/SME_labels/Biotech_Labels.tsv", sep="\t", index=False)
+
+df1 = pd.read_csv('/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/Examples/FDH/SME_labels/FDH_Labels.csv')
+df1.to_csv("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/Examples/FDH/SME_labels/FDH_Labels.tsv", sep="\t", index=False)
+
+os.remove("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/Examples/FDH/SME_labels/FDH_Labels.csv")
+os.remove("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/Examples/Biotech/SME_labels/Biotech_Labels.csv")
+
+# python3 python_scripts/csv_2_tsv.py
