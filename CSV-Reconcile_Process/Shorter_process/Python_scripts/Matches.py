06dd69b27a86070e48948cfaaee47375572da0a6
dorisavedikian <m>
2023-02-17 09:04:08 -0800

    update

--- CSV-Reconcile_Process/Shorter_process/Python_scripts/Matches.py
+++ CSV-Reconcile_Process/Shorter_process/Python_scripts/Matches.py
@@ -1,6 +1,6 @@
 
 '''
-- This edits the Matched output and saves it as an excel file
+- This edits the Matched and Unmatched OpenRefine output's (removes suffix, seperates based on score) and saves it as an excel file
 '''
 import os
 import csv
@@ -8,18 +8,21 @@ import pandas as pd
 import numpy as np
 import string
 
-df = pd.read_csv("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/Examples/FDH/Reconciled/label_matched.csv")
-df[['NALT_URI_Best_Match', '_randomint']] = df['Nalt_URI'].str.split('_', expand=True)         
-df1 = df.drop(['Nalt_URI','_randomint'], axis=1)    
-df1.to_excel("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/examples/FDH/reconciled/matched_reconciliation.xlsx", sheet_name='Perfect Matches', index = False)
+df1 = pd.read_csv("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/Examples/FDH/Reconciled/label_matched.csv")
+df2 = pd.read_csv("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/Examples/FDH/Reconciled/label_unmatched.csv")                   
 
-df2 = pd.read_csv("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/Examples/FDH/Reconciled/label_unmatched.csv")                                          
+df1[['NALT_URI_Best_Match', '_randomint']] = df1['Nalt_URI'].str.split('_', expand=True)         
+df_1 = df1.drop(['Nalt_URI','_randomint'], axis=1)                          
 df2[['NALT_URI_Best_Match', '_randomint']] = df2['Nalt_URI'].str.split('_', expand=True)         
-df3 = df2.drop(['Nalt_URI','_randomint'], axis=1)    
-df4 = df3.loc[df3['Score'] == 100] 
-df4.to_excel('/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/examples/FDH/reconciled/Unmatched_but_100%.xlsx', sheet_name='Look up Best_Match', index=False) 
-df5 = df3.loc[df3['Score'] < 100]    
-df5.to_excel("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/Examples/FDH/Reconciled/Labels_Not_In_NALT.xlsx", sheet_name = 'Not in Nalt', index=False) 
+df_2 = df2.drop(['Nalt_URI','_randomint'], axis=1)    
+df3 = df_1.loc[df_1['Score'] == 100] 
+df4 = df_2.loc[df_2['Score'] < 100]  
+
+writer = pd.ExcelWriter('/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/Examples/FDH/Reconciled/Reconciliation.xlsx', engine = 'xlswriter')
+df1.to_excel(writer, sheet_name='Matched & 100%', index = False)
+df3.to_excel(writer, sheet_name='Unmatched but 100%', index=False) 
+df4.to_excel(writer, sheet_name = 'Unmatched & < 100%', index=False) 
+writer.close()
 
 #os.remove("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/Examples/Biotech/Reconciled/label_unmatched.csv")
 #os.remove("/Volumes/USDA HD/NAL/MyGitFolder/Reconciliation_Project/CSV-Reconcile_Process/shorter_process/Examples/ADC/Reconciled/label_matched.csv")
